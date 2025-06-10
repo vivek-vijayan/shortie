323b624ec22d16cd0d90574009f1b1e43e31d356
vivekvijayan <vivekvijayan3@icloud.com>
2025-06-10 19:39:21 +0530

    Version 1.0 render push code

--- src/app.py
+++ src/app.py
@@ -45,87 +45,92 @@ async def index(request: Request):
 
 
 # Server Section
-@app.post("/generate", response_class=HTMLResponse)
+@app.post("/g", response_class=HTMLResponse)
 async def generate_shorturl(request: Request, org_url: str = Form(...)):
-    # Extract and wrap the original URL
-    url: str = org_url
-    url_obj = URL(str(url))
 
-    # Check if URL is not empty
-    if url != "":
-        # Attempt to connect to MongoDB
-        if connection == True:  # type: ignore
-            # Check if the original URL is already in the database
-            result = await mongoEngine.is_url_present( # type: ignore
-                url_obj.url, 
-                mongoEngine.get_collection("real_and_short_url_collection")  # type: ignore
-            )
+    if request.method == "POST":
+        # Extract and wrap the original URL
+        url: str = org_url
+        url_obj = URL(str(url))
 
-            # If the URL does not already exist, create a new short URL
-            if not result['exist']:
-                # Initialize hash generator with desired length
-                hashed_url = EncryptHash(6)
-
-                # Generate a short ID from the original URL
-                short_id = hashed_url.generate_short_id(url_obj.url)
-
-                # Ensure the short ID is not already in the Bloom filter (basic collision handling)
-                while filter_obj.check(value=short_id):
-                    short_id = hashed_url.generate_short_id(url_obj.url)
-                    filter_obj.add(short_id)
-
-                # Prepare the document to insert into the database
-                post_data: Dict[str, str] = {
-                    "original_url": url_obj.url,
-                    "short_url": short_id,
-                    "timestamp": str(time.ctime())
-                }
-
-                # Insert the data into MongoDB
-                await mongoEngine.insert_into_collection( # type: ignore
-                    data=post_data,
-                    collection_obj=mongoEngine.get_collection("real_and_short_url_collection")  # type: ignore
+        # Check if URL is not empty
+        if url != "":
+            # Attempt to connect to MongoDB
+            if connection == True:  # type: ignore
+                # Check if the original URL is already in the database
+                result = await mongoEngine.is_url_present( # type: ignore
+                    url_obj.url, 
+                    mongoEngine.get_collection("real_and_short_url_collection")  # type: ignore
                 )
 
-                # Return success response with new short URL
-                return templates.TemplateResponse(
-                        "short-url.html",
-                        context={
-                            'request': request,
-                            'original_url' : "",
-                            'short_url' : short_id
-                        }
+                # If the URL does not already exist, create a new short URL
+                if not result['exist']:
+                    # Initialize hash generator with desired length
+                    hashed_url = EncryptHash(6)
+
+                    # Generate a short ID from the original URL
+                    short_id = hashed_url.generate_short_id(url_obj.url)
+
+                    # Ensure the short ID is not already in the Bloom filter (basic collision handling)
+                    while filter_obj.check(value=short_id):
+                        short_id = hashed_url.generate_short_id(url_obj.url)
+                        filter_obj.add(short_id)
+
+                    # Prepare the document to insert into the database
+                    post_data: Dict[str, str] = {
+                        "original_url": url_obj.url,
+                        "short_url": short_id,
+                        "timestamp": str(time.ctime())
+                    }
+
+                    # Insert the data into MongoDB
+                    await mongoEngine.insert_into_collection( # type: ignore
+                        data=post_data,
+                        collection_obj=mongoEngine.get_collection("real_and_short_url_collection")  # type: ignore
                     )
 
-                
+                    # Return success response with new short URL
+                    return templates.TemplateResponse(
+                            "short-url.html",
+                            context={
+                                'request': request,
+                                'original_url' : "",
+                                'short_url' : short_id
+                            }
+                        )
+
+                    
+                else:
+                    # URL already exists in the database, return the existing short URL
+                    return templates.TemplateResponse(
+                            "short-url.html",
+                            context={
+                                'request': request,
+                                'original_url' : "",
+                                'short_url' : result.get('short_url')
+                            }
+                        )
             else:
-                # URL already exists in the database, return the existing short URL
-                return templates.TemplateResponse(
-                        "short-url.html",
-                        context={
-                            'request': request,
-                            'original_url' : "",
-                            'short_url' : result.get('short_url')
-                        }
-                    )
+                # No active connection to mongodb
+                return JSONResponse(
+                    content={
+                        'status': 'failed',
+                        'message': 'Connection to Database failed'
+                    },
+                    status_code=200
+                )
         else:
-            # No active connection to mongodb
+            # URL input was empty
             return JSONResponse(
                 content={
-                    'status': 'failed',
-                    'message': 'Connection to Database failed'
+                    'status': 'rejected',
+                    'message': 'URL is mandatory'
                 },
                 status_code=200
             )
+
     else:
-        # URL input was empty
-        return JSONResponse(
-            content={
-                'status': 'rejected',
-                'message': 'URL is mandatory'
-            },
-            status_code=200
-        )
+        return RedirectResponse(url="/", status_code=303)
 
 
 @app.get("/{short_url}")
@@ -140,6 +145,9 @@ async def get_original_url_and_redirect(short_url: str):
         RedirectResponse: Redirects to the original URL if found.
         JSONResponse: Error message if the short URL is invalid or DB is not connected.
     """
+
+    if short_url == "g":
+        return RedirectResponse(url="/", status_code=303)
     
     if connection:  # Ensure the DB connection is established
         result = await mongoEngine.get_original_url_from_short_url( # type: ignore
@@ -158,16 +166,10 @@ async def get_original_url_and_redirect(short_url: str):
             return RedirectResponse(url=url_to_redirect, status_code=301)
 
         # If not found, return a failure response
-        return JSONResponse(
-            content={'status': 'failed', 'message': 'Invalid short URL'},
-            status_code=404
-        )
+        return RedirectResponse(url="/", status_code=303)
 
     # If database is not connected
-    return JSONResponse(
-        content={'status': 'rejected', 'message': 'No active DB connection'},
-        status_code=503
-    )
+    return RedirectResponse(url="/", status_code=303)
 
 
 if __name__ == "__main__":
